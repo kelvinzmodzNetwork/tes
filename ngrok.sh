2bd8950557be9ce6f5041c04618f2bed4c314b57
kelvinzmodz <154925326+kelvinzmodzNetwork@users.noreply.github.com>
2024-11-15 02:35:23 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -36,7 +36,7 @@ sleep 5
 # Memastikan ngrok berjalan dan mendapatkan URL RDP
 echo "======================="
 echo "RDP Address:"
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:\/\/([^"]*).*/\1/p'
+curl --silent --show-error http://localhost:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:\/\/([^"]*).*/\1/p'
 echo "======================="
 
 # Mengunduh dan menjalankan Docker untuk RDP
